a62eb08c12e0dd4b055d010a0cf8c093d9a9737c
tongoonamujera <tongoonamujera@gmail.com>
2021-11-23 12:31:58 +0200

    Add classes to the project

--- js/index.js
+++ js/index.js
@@ -1,5 +1,2 @@
 import { addBook, displayBooks } from './booksView.js';
 
-const newBookForm = document.querySelector('#newBookForm');
-newBookForm.addEventListener('submit', addBook);
-displayBooks();
