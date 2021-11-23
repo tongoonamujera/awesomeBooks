2e12881dd7331c9f032e7fd1a1d05eed979f6b9c
tongoonamujera <tongoonamujera@gmail.com>
2021-11-23 15:14:43 +0200

    Fix linter errors in index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 import { Html, LocalStorage } from './booksClass.js';
 
 const newBookForm = document.querySelector('#newBookForm');
-newBookForm.addEventListener('submit', (e)=> {
+newBookForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  LocalStorage.addBook(e.target.title.value, e.target.author.value)
+  LocalStorage.addBook(e.target.title.value, e.target.author.value);
 });
 
 Html.displayBooks();
